7082c0938ef1a9aae78fdee2e14e7a0e05767c8b
mirkobechini <mirkobechini@gmail.com>
2025-10-14 20:54:04 +0200

    Add: added routes to pages AboutUs.jsx and Products.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Homepage from "./pages/Homepage"
+import AboutUsPage from "./pages/AboutUsPage"
+import ProductsPage from "./pages/ProductsPage"
 
 function App() {
 
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage/>} >
 
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+          <Route path="/aboutus" element={<AboutUsPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+
+
+            </Routes>
+          </BrowserRouter>
+
+          )
 }
 
 export default App
