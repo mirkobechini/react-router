cd722d5173e40506a2a1d65f2adefe0111953723
mirkobechini <mirkobechini@gmail.com>
2025-10-14 20:54:21 +0200

    Add: added Link in navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,9 @@
+import { NavLink } from "react-router-dom";
+
 export default function AppHeader() {
     return (
         <header>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-sm bg-body-tertiary">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">My E-commerce</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,13 +12,13 @@ export default function AppHeader() {
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <NavLink to="/" class="nav-link">Home</NavLink>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Chi siamo</a>
+                                <NavLink to="/aboutus" class="nav-link">Chi siamo</NavLink>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Prodotti</a>
+                                <NavLink to="/products" class="nav-link">Prodotti</NavLink>
                             </li>
                         </ul>
                     </div>
