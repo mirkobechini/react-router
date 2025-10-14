5c96953670583c14b11ec25bbd0282463652ca01
mirkobechini <mirkobechini@gmail.com>
2025-10-14 21:23:13 +0200

    Rename: in AppHeader class to className

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,22 +3,22 @@ import { NavLink } from "react-router-dom";
 export default function AppHeader() {
     return (
         <header>
-            <nav class="navbar navbar-expand-sm bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">My E-commerce</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-sm bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">My E-commerce</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <NavLink to="/" class="nav-link">Home</NavLink>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">Home</NavLink>
                             </li>
-                            <li class="nav-item">
-                                <NavLink to="/aboutus" class="nav-link">Chi siamo</NavLink>
+                            <li className="nav-item">
+                                <NavLink to="/aboutus" className="nav-link">Chi siamo</NavLink>
                             </li>
-                            <li class="nav-item">
-                                <NavLink to="/products" class="nav-link">Prodotti</NavLink>
+                            <li className="nav-item">
+                                <NavLink to="/products" className="nav-link">Prodotti</NavLink>
                             </li>
                         </ul>
                     </div>
