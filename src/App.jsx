894c9bc46f71a9c086ab30e833dc0675e7a221e9
mirkobechini <mirkobechini@gmail.com>
2025-10-14 20:40:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage/>} >
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import AppHeader from "../components/AppHeader";
+
+export default function Homepage() {
+
+    return (
+        <>
+            <AppHeader />
+            <main>
+                <div className="container my-3">
+
+                    <h2>Benvenuto nel mio e-commerce generato con l'api fakestore</h2>
+                </div>
+            </main>
+        </>
+    )
+}
