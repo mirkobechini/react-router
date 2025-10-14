0ba89e24c467da53e58136d89ce40d1ec79fb831
mirkobechini <mirkobechini@gmail.com>
2025-10-14 20:58:18 +0200

    Add: Create DefaultLayout.jsx and added to the routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import AboutUsPage from "./pages/AboutUsPage"
 import ProductsPage from "./pages/ProductsPage"
+import DefaultLayout from "./layouts/DefaultLayout"
 
 function App() {
 
@@ -12,15 +13,17 @@ function App() {
 
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route element={<DefaultLayout/>} >
+          <Route index element={<Homepage />} />
           <Route path="/aboutus" element={<AboutUsPage />} />
-            <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+        </Route>
 
 
-            </Routes>
-          </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
 
-          )
+  )
 }
 
 export default App
