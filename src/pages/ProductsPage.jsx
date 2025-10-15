1b7d12d89ca22b67fabb7c26394ed1caaa334985
mirkobechini <mirkobechini@gmail.com>
2025-10-15 19:31:53 +0200

    Style: adjust product card layout and image display in ProductsPage component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -26,7 +26,7 @@ export default function ProductsPage({ productsEP }) {
     return (
         <>
             <div className="container">
-                <div className="row row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
+                <div className="row row-cols-sm-2 row-cols-md-4 row-cols-lg-5">
 
                     {products.map(product =>
                         <div className="col my-3" key={product.id}>
@@ -34,13 +34,17 @@ export default function ProductsPage({ productsEP }) {
                                 <div className="card-header">
                                     <h3 className="text-truncate d-block">{product.title}</h3>
                                 </div>
-                                <div className="card-body position-relative">
-                                    <p className="text-truncate text-hidden">
+                                <div className="ratio ratio-1x1 my-3">
+
+                                    <img src={product.image} className="card-img-top" style={{objectFit:"contain"}}></img>
+                                </div>
+                                <div className="card-body   position-relative">
+                                    <div className="badge btn btn-primary position-absolute top-0 start-0"> {product.category}</div>
+                                    <p className="text-truncate text-hidden mt-3">
                                         {product.description}
                                     </p>
                                     <span className="d-block"> <i className="bi bi-currency-euro"></i>{product.price} </span>
                                     <span className="d-block text-warning"> <i className="bi bi-star-fill me-1"></i> {product.rating.rate}</span>
-                                    <div className="badge btn btn-primary position-absolute top-0 end-0"> {product.category}</div>
                                 </div>
                             </div>
                         </div>
