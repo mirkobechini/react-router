78af61dd632687feffe776e1399891b0ca836ce6
mirkobechini <mirkobechini@gmail.com>
2025-10-15 19:33:44 +0200

    Refactor: move product card rendering to separate ProductCard component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 
 export default function ProductsPage({ productsEP }) {
@@ -30,23 +31,7 @@ export default function ProductsPage({ productsEP }) {
 
                     {products.map(product =>
                         <div className="col my-3" key={product.id}>
-                            <div className="card h-100  ">
-                                <div className="card-header">
-                                    <h3 className="text-truncate d-block">{product.title}</h3>
-                                </div>
-                                <div className="ratio ratio-1x1 my-3">
-
-                                    <img src={product.image} className="card-img-top" style={{objectFit:"contain"}}></img>
-                                </div>
-                                <div className="card-body   position-relative">
-                                    <div className="badge btn btn-primary position-absolute top-0 start-0"> {product.category}</div>
-                                    <p className="text-truncate text-hidden mt-3">
-                                        {product.description}
-                                    </p>
-                                    <span className="d-block"> <i className="bi bi-currency-euro"></i>{product.price} </span>
-                                    <span className="d-block text-warning"> <i className="bi bi-star-fill me-1"></i> {product.rating.rate}</span>
-                                </div>
-                            </div>
+                            <ProductCard product={product}/>
                         </div>
                     )}
                 </div>
