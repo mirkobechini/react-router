bccbff05acd685c8bc1848d96bf653b558fb58ed
mirkobechini <mirkobechini@gmail.com>
2025-10-15 19:45:57 +0200

    Style: clean up whitespace and improve text truncation in ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,16 @@
 export default function ProductCard({ product }) {
     return (
         <>
-            <div className="card h-100  ">
+            <div className="card h-100">
                 <div className="card-header">
                     <h3 className="text-truncate d-block">{product.title}</h3>
                 </div>
                 <div className="ratio ratio-1x1 my-3">
-
                     <img src={product.image} className="card-img-top" style={{ objectFit: "contain" }}></img>
                 </div>
-                <div className="card-body   position-relative">
+                <div className="card-body position-relative">
                     <div className="badge btn btn-primary position-absolute top-0 start-0"> {product.category}</div>
-                    <p className="text-truncate text-hidden mt-3">
+                    <p className="overflow-hidden mt-3" style={{ maxHeight: "4.5rem" }}>
                         {product.description}
                     </p>
                     <span className="d-block"> <i className="bi bi-currency-euro"></i>{product.price} </span>
