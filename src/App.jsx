00a5e5d8b380419a37fc93899ff1a99ad11cf58c
mirkobechini <mirkobechini@gmail.com>
2025-10-18 15:58:57 +0200

    Add: include ProductPage route in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import AboutUsPage from "./pages/AboutUsPage"
 import ProductsPage from "./pages/ProductsPage"
+import ProductPage from "./pages/ProductPage"
 import DefaultLayout from "./layouts/DefaultLayout"
 
 //api
@@ -21,6 +22,7 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="/aboutus" element={<AboutUsPage />} />
           <Route path="/products" element={<ProductsPage productsEP={productsEP} />} />
+          <Route path="/products/:id" element={<ProductPage/>} />
         </Route>
 
 
